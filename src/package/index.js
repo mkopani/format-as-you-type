711042758485caf6423f64fc77c87219ddcbb547
Mark Kopani <mkopani@gmail.com>
2022-01-26 10:10:11 -0800

    Renamed 'dist' folder to 'package'

---
+++ src/package/index.js
@@ -0,0 +1,17 @@
+import formatBigNumber from "./formatBigNumber";
+import formatCreditCard from "./formatCreditCard";
+import formatDate from "./formatDate";
+import formatPhone from "./formatPhone";
+import formatPostalCodeCanada from "./formatPostalCodeCanada";
+import formatString from "./formatString";
+import useFieldFormatter from "./useFieldFormatter";
+
+export {
+  formatBigNumber,
+  formatCreditCard,
+  formatDate,
+  formatPhone,
+  formatPostalCodeCanada,
+  formatString,
+  useFieldFormatter,
+};
