8343713b7399ca4fceac8da187ea0fbbceda0830
Mark Kopani <mkopani@gmail.com>
2022-01-26 11:35:47 -0800

    Added index.js to _lib/ and filters/; updated .gitignore

---
+++ src/package/_lib/index.js
@@ -0,0 +1,17 @@
+import findIndexAfterMovement from "./findIndexAfterMovement";
+import handleBackspace from "./handleBackspace";
+import isLetter from "./isLetter";
+import isNumber from "./isNumber";
+import isSeparator from "./isSeparator";
+import removeCharacters from "./removeCharacters";
+import setDefaultOption from "./setDefaultOption";
+
+export {
+  findIndexAfterMovement,
+  handleBackspace,
+  isLetter,
+  isNumber,
+  isSeparator,
+  removeCharacters,
+  setDefaultOption,
+};
---
+++ src/package/filters/index.js
@@ -0,0 +1,5 @@
+import removeLetters from "./removeLetters";
+import removeNumbers from "./removeNumbers";
+import removeSeparators from "./removeSeparators";
+
+export { removeLetters, removeNumbers, removeSeparators };
